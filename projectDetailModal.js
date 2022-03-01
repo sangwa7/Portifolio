ee354345e209eaddcb253df18b352499b81b0854
sangwa7 <didiersangwa@gmail.com>
2022-03-01 19:42:14 +0200

    Add layout for project detail layout

--- projectDetailModal.js
+++ projectDetailModal.js
@@ -1,11 +1,11 @@
 function projectDetail() {
-    var elements = document.getElementsByClassName('project-detail');
-    Array.from(elements).forEach((x) => {
-        if (x.style.display === "block") {
-            x.style.display = "none";
-          } else {
-            x.style.display = "block";
-          }
-    })
-    console.log("See project clicked");
-  }
+  const elements = document.getElementsByClassName('project-detail');
+  Array.from(elements).forEach((x) => {
+    if (x.style.display === 'block') {
+      x.style.display = 'none';
+    } else {
+      x.style.display = 'block';
+    }
+  });
+}
+projectDetail();
