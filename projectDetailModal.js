0fb09d7c252314df573b2784e95ccdc3f832c5b5
sangwa7 <didiersangwa@gmail.com>
2022-03-02 19:37:18 +0200

    Add project detail from JS object

--- projectDetailModal.js
+++ projectDetailModal.js
@@ -9,3 +9,37 @@ function projectDetail() {
   });
 }
 projectDetail();
+
+const textP = document.querySelector('.project-detail-text-p');
+const multiPostStories = document.querySelector('.project-detail-multi-post-stories');
+
+const projectDetails = [
+  {
+    title: 'Multi Post Stories 123',
+    image: 'Snapshoot-Portfolio.svg',
+    mageAlt: '',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy textever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    technologies: ['html', 'css', 'JavaScript'],
+    sourceCode: 'https://github.com/sangwa7/Portifolio',
+    seeLive: 'https://sangwa7.github.io/Portifolio/',
+  },
+];
+
+const imageDirectory = 'img/';
+let ProjectDetailText = '';
+let ProjectDetailTitle = '';
+
+Array.from(projectDetails).forEach((project) => {
+  ProjectDetailText += project.text;
+  ProjectDetailTitle += project.title;
+  const image = document.getElementById('project-img');
+  image.src = imageDirectory + project.image;
+  image.alt = imageDirectory + project.imageAlt;
+  const seeLive = document.getElementById('project-detail-button-1');
+  seeLive.href = project.seeLive;
+  const source = document.getElementById('project-detail-button-2');
+  source.href = project.sourceCode;
+});
+
+textP.innerHTML = ProjectDetailText;
+multiPostStories.innerHTML = ProjectDetailTitle;
