3ccf8aa91429c30c43849fbc49321d7d7c9c358a
Edward Odhiambo <odhiambo.edward111@gmail.com>
2022-03-03 13:37:44 +0300

    Add Form Validation JS

--- formValidation.js
+++ formValidation.js
@@ -1,21 +1,19 @@
-function validate() {
-  if (document.myForm.Name.value === '') {
-    alert('Please provide your name!');
-    document.myForm.Name.focus();
-    return false;
-  }
-  if (document.myForm.EMail.value === '') {
-    const emailID = document.myForm.EMail.value;
-    const atpos = emailID.indexOf('@');
-    const dotpos = emailID.lastIndexOf('.');
+// Form validation
+ document.querySelector("#contact-form").addEventListener("submit", e =>{
+  let valid = true;
 
-    if (atpos < 1 || dotpos - atpos < 2) {
-      alert('Please enter correct email ID');
-      document.myForm.EMail.focus();
-      return false;
+  let responseContainer = document.querySelector("#warning-message");
+
+  let emailArray = document.querySelector("#email").value.split('');
+
+  for(let i = 0; i < emailArray.length; i++){
+    if(emailArray[i] != emailArray[i].toLowerCase() && emailArray[i] != "@" && emailArray[i] != ".") {
+      valid = false;
     }
   }
-  return true;
-}
 
-validate();
+  if(!valid){
+    e.preventDefault();
+    responseContainer.innerHTML = "Invalid Email";
+  }
+});
